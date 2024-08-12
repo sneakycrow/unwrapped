9a03450e5b29b9ce59b3bf053330a45246a77806
Zachary Corvidae <zach@sneakycrow.dev>
2024-08-12 14:58:31 -0700

    add token refresh logic

--- src/music/spotify.rs
+++ src/music/spotify.rs
@@ -1,10 +1,12 @@
+use base64::prelude::*;
 use serde::{Deserialize, Serialize};
-use tracing::error;
+use surf::http::mime;
+use tracing::{debug, error};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SpotifyError {
-    status: u16,
-    message: String,
+    pub status: u16,
+    pub message: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -49,7 +51,8 @@ pub struct RecentTrack {
 
 /// The primary client for interacting with the Spotify API
 pub struct SpotifyClient {
-    pub token: String,
+    pub access_token: String,
+    pub refresh_token: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -58,32 +61,114 @@ pub struct RecentTracksResponse {
     pub error: Option<SpotifyError>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RefreshTokenResponse {
+    pub access_token: String,
+    pub token_type: String,
+    pub scope: String,
+    pub expires_in: u32,
+}
+
 impl SpotifyClient {
+    /// Create a new SpotifyClient with an access token
     pub fn new(access_token: String) -> Self {
         SpotifyClient {
-            token: access_token,
+            access_token,
+            refresh_token: None,
         }
     }
-
+    /// Set the refresh token for the client
+    pub fn set_refresh_token(mut self, refresh_token: Option<String>) -> Self {
+        self.refresh_token = refresh_token;
+        self
+    }
+    /// Set the access token for the client
+    pub fn set_access_token(mut self, access_token: String) -> Self {
+        self.access_token = access_token;
+        self
+    }
+    /// Get a new access token using the refresh token
+    pub async fn refresh_access_token(&self) -> Result<RefreshTokenResponse, SpotifyError> {
+        // If a refresh_token does not exist, we cannot get a new access token
+        let refresh_token = match &self.refresh_token {
+            Some(token) => token,
+            None => {
+                return Err(SpotifyError {
+                    status: 400,
+                    message: "No refresh token provided".to_string(),
+                })
+            }
+        };
+        // Get new token from Spotify
+        let new_token = Self::request_access_token(refresh_token.clone())
+            .await
+            .map_err(|err| {
+                error!("Failed to fetch new access token from Spotify {:?}", err);
+                SpotifyError {
+                    status: 500,
+                    message: "Internal error requesting access token from Spotify".to_string(),
+                }
+            })?;
+        // Return the new access token and refresh token
+        Ok(new_token)
+    }
+    /// Fetch the recent tracks from Spotify
     pub async fn get_recent_tracks(&self) -> Result<RecentTracksResponse, SpotifyError> {
+        debug!(
+            "Fetching recent tracks from Spotify using access token {}",
+            self.access_token
+        );
         const ENDPOINT: &'static str = "https://api.spotify.com/v1/me/player/recently-played";
         let tracks: RecentTracksResponse = surf::get(ENDPOINT)
-            .header("Authorization", format!("Bearer {}", self.token))
+            .header("Authorization", format!("Bearer {}", self.access_token))
             .recv_json()
             .await
             .map_err(|err| {
-                error!("Failed to get recent tracks from Spotify: {:?}", err);
+                error!("Failed to fetch json from spotify {:?}", err);
                 SpotifyError {
                     status: 500,
                     message: "Internal error requesting recent tracks from Spotify".to_string(),
                 }
             })?;
-        // If we received an error from Spotify, it should still parse as an Ok result
-        // but we still represent this as an error, just a recoverable parsed error
+        // The request can successfully parse the JSON response, but the response itself can contain
+        // an error from Spotify's API. If an error is present, return the error
         if let Some(error) = tracks.error {
-            error!("Spotify API returned an error: {:?}", error);
             return Err(error);
         }
         Ok(tracks)
+    }
+    /// Send request to Spotify to refresh the access token
+    pub(crate) async fn request_access_token(
+        refresh_token: String,
+    ) -> Result<RefreshTokenResponse, SpotifyError> {
+        const ENDPOINT: &'static str = "https://accounts.spotify.com/api/token";
+        let client_id = std::env::var("SPOTIFY_ID").map_err(|_| SpotifyError {
+            status: 500,
+            message: "Missing Spotify Client ID".to_string(),
+        })?;
+        let client_secret = std::env::var("SPOTIFY_SECRET").map_err(|_| SpotifyError {
+            status: 500,
+            message: "Missing Spotify Client Secret".to_string(),
+        })?;
+        let auth = BASE64_STANDARD.encode(format!("{}:{}", client_id, client_secret));
+        let body = format!("grant_type=refresh_token&refresh_token={}", refresh_token);
+        let token: RefreshTokenResponse = surf::post(ENDPOINT)
+            .header("Authorization", format!("Basic {}", auth))
+            .content_type(mime::FORM)
+            .body(body)
+            .recv_json()
+            .await
+            .map_err(|err| {
+                error!("Failed to fetch json from spotify {:?}", err);
+                SpotifyError {
+                    status: 500,
+                    message: "Internal error requesting access token from Spotify".to_string(),
+                }
+            })?;
+        debug!(
+            "Successfully fetched new access token from Spotify {}",
+            token.access_token
+        );
+        Ok(token)
     }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -7,10 +7,13 @@ use serde::{Deserialize, Serialize};
 
 use axum::{
     extract::{Query, State},
+    http::StatusCode,
     response::Html,
     routing::get,
     Json, Router,
 };
+
+use tracing::{debug, error};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -39,24 +42,77 @@ async fn index() -> Html<String> {
 #[derive(Serialize)]
 pub struct CollectionResponse {
     data: Vec<RecentTrack>,
+    updated_token: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
 struct Tokens {
     access_token: String,
+    refresh_token: Option<String>,
 }
 
 async fn collect(
     State(_state): State<AppState>,
     tokens: Query<Tokens>,
-) -> Json<CollectionResponse> {
+) -> Result<Json<CollectionResponse>, (StatusCode, String)> {
+    // Collect the tokens from the query
     let access_token = tokens.access_token.to_owned();
-    let response = SpotifyClient::new(access_token)
-        .get_recent_tracks()
-        .await
-        .expect("Failed to collect spotify data");
-
-    Json(CollectionResponse {
-        data: response.items.unwrap_or(vec![]),
-    })
+    let refresh_token = tokens.refresh_token.to_owned();
+    // Generate a client for interacting with Spotify
+    let client = SpotifyClient::new(access_token).set_refresh_token(refresh_token);
+    // Fetch the recent tracks from Spotify
+    let recent_tracks_response = client.get_recent_tracks().await;
+    match recent_tracks_response {
+        Ok(recent_tracks) => {
+            debug!("Successfully collected spotify data");
+            Ok(Json(CollectionResponse {
+                data: recent_tracks.items.unwrap_or_default(),
+                updated_token: None,
+            }))
+        }
+        Err(spotify_err) => {
+            match spotify_err.status {
+                // If the error is an invalid token error, try to get a new access token
+                401 => {
+                    debug!("Invalid Token error, Attempting to get a new access token");
+                    // First get the new access token
+                    let new_token = client.refresh_access_token().await.map_err(|err| {
+                        error!(
+                            "Error trying to recover from failed access token: {:?}",
+                            err
+                        );
+                        (
+                            StatusCode::UNPROCESSABLE_ENTITY,
+                            "Could not refresh access token".to_string(),
+                        )
+                    })?;
+                    // Update the client with the new access token and try to get the recent tracks again
+                    let recent_tracks = client
+                        .set_access_token(new_token.access_token.clone())
+                        .get_recent_tracks()
+                        .await
+                        .map_err(|err| {
+                            error!("Error collecting spotify data after recovery: {:?}", err);
+                            (
+                                StatusCode::INTERNAL_SERVER_ERROR,
+                                "Error collecting spotify data".to_string(),
+                            )
+                        })?;
+                    // Since we updated the access token, return it as well so the client can update it
+                    Ok(Json(CollectionResponse {
+                        data: recent_tracks.items.unwrap_or_default(),
+                        updated_token: Some(new_token.access_token),
+                    }))
+                }
+                // Any other errors aren't specifically handled, so we log the error and return a 500
+                _ => {
+                    error!("Error collecting spotify data: {:?}", spotify_err);
+                    Err((
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        "Error collecting spotify data".to_string(),
+                    ))
+                }
+            }
+        }
+    }
 }
