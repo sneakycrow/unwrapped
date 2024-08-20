432fcb126a9682e771636d17a24bcd04bd1962d9
Zachary Corvidae <zach@sneakycrow.dev>
2024-08-19 20:48:08 -0700

    add user/account migrations

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -4,6 +4,8 @@ mod m20240813_164238_init_artists;
 mod m20240813_170813_init_albums;
 mod m20240813_170819_init_tracks;
 mod m20240813_170827_init_playlog;
+mod m20240820_031732_init_users;
+mod m20240820_031738_init_accounts;
 
 pub struct Migrator;
 
@@ -15,6 +17,8 @@ impl MigratorTrait for Migrator {
             Box::new(m20240813_170813_init_albums::Migration),
             Box::new(m20240813_170819_init_tracks::Migration),
             Box::new(m20240813_170827_init_playlog::Migration),
+            Box::new(m20240820_031732_init_users::Migration),
+            Box::new(m20240820_031738_init_accounts::Migration),
         ]
     }
 }
