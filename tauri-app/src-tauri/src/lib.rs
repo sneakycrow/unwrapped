321aaf5e505c196e1cdba8d14a44c3c829ea2b21
Zachary Corvidae <zach@sneakycrow.dev>
2024-09-19 14:44:32 -0700

    more tauri wip

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Listener;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,9 +8,29 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
+    #[allow(unused_mut)]
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|_app, argv, _cwd| {
+            println!("single instance triggered: {argv:?}");
+        }));
+    }
+    builder
+        .plugin(tauri_plugin_deep_link::init())
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .level(log::LevelFilter::Info)
+                .build()
+        )
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            app.listen("deep-link://new-url", |url| {
+                dbg!(url);
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- tauri-app/src/main.tsx
+++ tauri-app/src/main.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {
+  onOpenUrl,
+  getCurrent as getCurrentDeepLinkUrls,
+} from "@tauri-apps/plugin-deep-link";
+
+function handler(urls: string[]) {
+  console.log(urls);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  onOpenUrl(handler);
+
+  getCurrentDeepLinkUrls()
+    .then((res) => {
+      console.log("res", res);
+    })
+    .catch(console.error);
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
