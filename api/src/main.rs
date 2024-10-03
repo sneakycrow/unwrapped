4138245122fe0cee6e9f46afaa643dea6a7706a1
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-03 12:09:17 -0700

    Fix log reference to old package name

--- api/src/main.rs
+++ api/src/main.rs
@@ -19,7 +19,7 @@ async fn main() {
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
                 // axum logs rejections from built-in extractors with the `axum::rejection`
                 // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
-                "unwrapped=debug,tower_http=debug,axum::rejection=trace".into()
+                "api=debug,tower_http=debug,axum::rejection=trace".into()
             }),
         )
         .with(tracing_subscriber::fmt::layer())
